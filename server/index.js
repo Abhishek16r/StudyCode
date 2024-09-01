73187dc9dab65b260967dbd1dc3bcb473c8a00b6
Abhishek Rai <97181654+Abhishek16r@users.noreply.github.com>
2024-09-01 15:50:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://studycode-backend.onrender.com/",
 		credentials:true,
 	})
 )
