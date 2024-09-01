92d871c8961b9336743a11d44c4981a76261021d
Abhishek Rai <97181654+Abhishek16r@users.noreply.github.com>
2024-09-01 16:24:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,24 +24,11 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        'https://studycode.vercel.app',
-        'https://studycode-fi6fp6kuu-abhishek-s-projects-e04082ac.vercel.app',
-      ];
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  })
-);
-
-// Handle preflight request
-app.options('*', cors());
+	cors({
+		origin:"http://localhost:3000",
+		credentials:true,
+	})
+)
 
 app.use(
 	fileUpload({
